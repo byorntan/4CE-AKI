8ef9271ccf6ffe59921b7eeafe8243544249598d
byorntan <68698155+byorntan@users.noreply.github.com>
2020-08-01 23:27:16 +0800

    v0.3

--- figure1_cr_trends.R
+++ figure1_cr_trends.R
@@ -12,31 +12,32 @@ library(dplyr)
 # This analysis uses the index AKI episode
 # Feel free to modify the code to look at other types of AKI episodes, e.g. you can filter for the most severe AKI episode
 # First, filter the labs_aki_severe table to show only the index AKI episodes
-aki_index <- labs_aki_severe %>% group_by(patient_id) %>% filter(days_since_admission == min(days_since_admission)) %>% ungroup()
+aki_only_index <- labs_aki_severe %>% group_by(patient_id) %>% filter(days_since_admission == min(days_since_admission)) %>% ungroup()
 # patient_id	site_id	days_since_admission	value	day_min	day_min_retro	min_cr_7day	min_cr_48h	min_cr_7day_retro	min_cr_48h_retro	min_cr_7d_final	cr_7d	cr_90d	delta_cr	aki_kdigo	aki_kdigo_retro	aki_kdigo_final	akd_7d	akd_90d	time_to_severe	severe_to_aki	severe_before_aki
 
 # Generate the patient list including (1) severity indices from this filtered table (2) day of peak Cr
 # severe - 2 = never severe, 4 = severe, AKI before severity onset, 5 = severe, AKI after severity onset
-aki_index <- aki_index %>% group_by(patient_id) %>% mutate(severe = ifelse(time_to_severe != -999,ifelse(severe_before_aki == 1,5,4),2)) %>% ungroup()
-aki_index <- aki_index[,c(1,3,23,5)]
-colnames(aki_index)[2] <- "peak_cr_time"
-colnames(aki_index)[4] <- "aki_start"
-# Headers of aki_index: patient_id  peak_cr_time  severe  aki_start
+aki_only_index <- aki_only_index %>% group_by(patient_id) %>% mutate(severe = ifelse(time_to_severe != -999,ifelse(severe_before_aki == 1,5,4),2)) %>% ungroup()
+aki_only_index <- aki_only_index[,c(1,3,23,5,21)]
+colnames(aki_only_index)[2] <- "peak_cr_time"
+colnames(aki_only_index)[4] <- "aki_start"
+# Headers of aki_only_index: patient_id  peak_cr_time  severe  aki_start  severe_to_aki
 
 no_aki_list <- demographics_filt[,c(1,7)]
 no_aki_list <- no_aki_list[!(no_aki_list$patient_id %in% aki_index$patient_id),]
 no_aki_list <- no_aki_list %>% group_by(patient_id) %>% mutate(severe = ifelse(severe == 1,3,1))
 no_aki_list$peak_cr_time <- 0
 no_aki_list$aki_start <- 0
-no_aki_list <- no_aki_list[,c(1,3,2,4)]
+no_aki_list$severe_to_aki <- -999
+no_aki_list <- no_aki_list[,c(1,3,2,4,5)]
 
-aki_index <- bind_rows(aki_index,no_aki_list)
+aki_index <- bind_rows(aki_only_index,no_aki_list)
 
 # Now bind this list of days of peak Cr to a new table containing the Cr trends
 peak_aki_trend <- labs_cr_aki[,c(1,3,4,5,7)]
 peak_aki_trend <- merge(peak_aki_trend,aki_index,by="patient_id",all.x=TRUE)
-peak_aki_trend <- peak_aki_trend[,c(1,7,2,6,3,4,5)]
-# patient_id  severe  days_since_admission  peak_cr_time  value min_cr_7day min_cr_7day_retro
+peak_aki_trend <- peak_aki_trend %>% select(patient_id,severe,days_since_admission,peak_cr_time,value,min_cr_7day,min_cr_7day_retro)
+# patient_id  severe  days_since_admission  peak_cr_time  value min_cr_7day min_cr_7day_retro severe_to_aki
 
 # Calculate the day from peak Cr
 peak_aki_trend <- peak_aki_trend %>% group_by(patient_id) %>% mutate(time_from_peak = days_since_admission - peak_cr_time) %>% ungroup()
@@ -45,7 +46,7 @@ peak_aki_trend <- peak_aki_trend %>% group_by(patient_id) %>% filter(between(tim
 # Normalise to baseline values used for AKI calculation
 peak_aki_trend <- peak_aki_trend %>% group_by(patient_id) %>% mutate(baseline_cr = min(min_cr_7day,min_cr_7day_retro)) %>% ungroup()
 peak_aki_trend <- peak_aki_timeplot %>% group_by(patient_id) %>% mutate(ratio = value/baseline_cr) %>% ungroup()
-peak_aki_trend <- peak_aki_trend[,c(1,2,8,10)]
+peak_aki_trend <- peak_aki_trend[,c(1,2,9,11)]
 # Headers: patient_id  severe  time_from_peak  ratio
 
 # Calculate mean and SD each for severe and non-severe groups
